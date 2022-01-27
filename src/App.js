a627d998c8fd04b7f33fc067a0a778a5672cddab
Mustafizur <sayhemrahman@gmail.com>
2022-01-27 16:46:19 +0600

    added firebase and authprovider with context

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import DashBoard from './components/DashBoard/DashBoard';
 import Home from './components/Home/Home';
+import AuthProvider from './Context/AuthProvider';
 
 function App() {
   return (
     <div className="App">
-      {/* <Home></Home> */}
-      <DashBoard></DashBoard>
+      <AuthProvider>
+
+        {/* <Home></Home> */}
+        {/* <DashBoard></DashBoard> */}
+
+      </AuthProvider>
     </div>
   );
 }
