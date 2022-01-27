6e758cf7701f52e8aa0c362ab45087b58c476eb4
Mustafizur <sayhemrahman@gmail.com>
2022-01-27 16:40:09 +0600

    added dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import DashBoard from './components/DashBoard/DashBoard';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      {/* <Home></Home> */}
+      <DashBoard></DashBoard>
     </div>
   );
 }
